a290add7c5a96445c09ffedf25f0ad98525e5fe3
Karl Amoguis <ktamoguis@gmail.com>
2018-05-06 09:21:53 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,18 @@
 class Triangle
   # write code here
   attr_reader :side1, :side2, :side3
-  
+
   def initialize(side1, side2, side3)
     @side1 = side1
     @side2 = side2
     @side3 = side3
   end
+
+  def kind
+
+
+  class TriangleError < StandardError
+    # triangle error code
+  end  
+  
 end
